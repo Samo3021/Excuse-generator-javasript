6ed508aea741fa77feeb9b591b10db9c4aba91b4
Samo3021 <samuelcampos15009@gmail.com>
2021-01-27 16:05:37 +0000

    arreglo.length

--- scrip.js
+++ scrip.js
@@ -1,11 +1,11 @@
 let who = ['The dog','My grandma','His turtle','My bird']; 
-let parte1 = Math.floor(Math.random()* 4);
+let parte1 = Math.floor(Math.random()* who.length);
 let action = ['ate','peed','crushed','broke']
-let parte2 = Math.floor(Math.random()* 4);
+let parte2 = Math.floor(Math.random()* action.length);
 let what = ['my homework', 'the keys', 'the car']
-let parte3= Math.floor(Math.random()* 3);
+let parte3= Math.floor(Math.random()* who.length);
 let when = ['before the class','right on time','when I finished','during my lunch','while I was praying']
-let parte4 = Math.floor(Math.random()* 3);
+let parte4 = Math.floor(Math.random()* who.length);
  
 document.querySelector("#excuse").innerHTML= who[parte1] + "" + action[parte2] + "" + what[parte3] + "" + when[parte4];
 
